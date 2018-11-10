71e3e7265623df11cbdd6d4877075f7d0d92b270
Kiss, Aron <aron123dev@gmail.com>
2018-11-10 16:42:18 +0100

    Add 'browse manufacturers' view

--- js/main.js
+++ js/main.js
@@ -1,9 +1,45 @@
 function init () {
     $('#menubar').load('menuBar.html');
-    //$('#content').load('');
     $('#footer').load('footer.html');
 }
 
+/**
+ * Browse manufacturers
+ */
+
+function fillManufacturersTable (data, status) {
+    if (status != "success") {
+        return;
+    }
+
+    for (let i in data) {
+        $('#manufacturers-table').append(
+            `<tr>
+                <td>${data[i].name}</td>
+                <td>${data[i].country}</td>
+                <td>${data[i].founded}</td>
+            </tr>`
+        );
+    }
+}
+
+function loadManufacturersData () {
+    $.get("/manufacturers", fillManufacturersTable)
+}
+
+function loadBrowseManufacturers () {
+    $('#content').load('browseManufacturers.html');
+    loadManufacturersData();
+}
+
+/**
+ * 
+ */
+
+
+/**
+ * Page initialization
+ */
 function main () {
     $(document).ready(init);
 }
